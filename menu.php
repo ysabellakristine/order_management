fc07368120a2c27be36fa899605e2c0e456aa9b2
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-05-19 17:11:36 +0800

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,7 @@
+<?php
+$candy = [
+    'Toffee' => ['price' => 3, 'stock' => 12, 'description' => 'Delicious toffee candy'],
+    'Mints'  => ['price' => 2, 'stock' => 26, 'description' => 'Refreshing mint candy'],
+    'Fudge'  => ['price' => 4, 'stock' => 8, 'description' => 'Rich and creamy fudge candy'],
+];
+?>
