a257ba8f8d7d7cff3e72c6733480dca7e0468969
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-05-19 21:06:20 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <?php
 $menu_item = [
-    'Mushroom Burger' => ['price' => 47.5, 'stock' => 99, 'description' => 'Best burger'],
-    'Cheese Burger'  => ['price' => 45, 'stock' => 99, 'description' => 'Burger with cheese'],
-    'Normal Burger'  => ['price' => 40, 'stock' => 99, 'description' => 'Plain burger for plain people'],
-    'Hawaiian Burger'  => ['price' => 70, 'stock' => 99, 'description' => 'For the ones who like pineapple pizza'],
-    'Borgor'  => ['price' => 30, 'stock' => 99, 'description' => 'If you buy this you have dyslexia'],
-    'Surprise drink'  => ['price' => 10, 'stock' => 99, 'description' => 'Random Drink: coke, sprite, pepsi'],
+    'Mushroom Burger' => ['price' => 47.5, 'description' => 'Best burger'],
+    'Cheese Burger'  => ['price' => 45, 'description' => 'Burger with cheese'],
+    'Normal Burger'  => ['price' => 40, 'description' => 'Plain burger for plain people'],
+    'Hawaiian Burger'  => ['price' => 70,  'description' => 'For the ones who like pineapple pizza'],
+    'Borgor'  => ['price' => 30, 'description' => 'If you buy this you have dyslexia'],
+    'Surprise drink'  => ['price' => 10,  'description' => 'Random Drink: coke, sprite, pepsi'],
 ];
 ?>
