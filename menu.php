0a5cfffb94abd4b89eab6555322240777b607c7a
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-05-19 17:20:41 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,10 @@
 <?php
-$candy = [
-    'Toffee' => ['price' => 3, 'stock' => 12, 'description' => 'Delicious toffee candy'],
-    'Mints'  => ['price' => 2, 'stock' => 26, 'description' => 'Refreshing mint candy'],
-    'Fudge'  => ['price' => 4, 'stock' => 8, 'description' => 'Rich and creamy fudge candy'],
+$menu_item = [
+    'Mushroom Burger' => ['price' => 47.5, 'stock' => 99, 'description' => 'Best burger'],
+    'Cheese Burger'  => ['price' => 45, 'stock' => 99, 'description' => 'Burger with cheese'],
+    'Normal Burger'  => ['price' => 40, 'stock' => 99, 'description' => 'Plain burger for plain people'],
+    'Hawaiian Burger'  => ['price' => 70, 'stock' => 99, 'description' => 'For the ones who like pineapple pizza'],
+    'Borgor'  => ['price' => 30, 'stock' => 99, 'description' => 'If you buy this you have dyslexia'],
+    'Surprise drink'  => ['price' => 10, 'stock' => 99, 'description' => 'Random Drink: coke, sprite, pepsi'],
 ];
 ?>
